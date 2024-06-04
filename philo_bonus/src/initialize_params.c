de483dfb94270b1b38d65dd82e948f6f1b5fc9d3
fernandezclaudia <cfernandezg@bcds.net>
2024-06-04 10:52:15 +0200

    closing and freeing

--- philo_bonus/src/initialize_params.c
+++ philo_bonus/src/initialize_params.c
@@ -1,7 +1,7 @@
 
 #include "../include/philosophers_bonus.h"
 
-static void	initializes_semaphores(t_options *opt)
+static int	initializes_semaphores(t_options *opt)
 {
 	sem_unlink("/forks");
 	sem_unlink("/write");
@@ -9,6 +9,9 @@ static void	initializes_semaphores(t_options *opt)
 	opt->forks = sem_open("/forks", O_CREAT, S_IRWXU, opt->num_philo);
 	opt->write = sem_open("/write", O_CREAT, S_IRWXU, 1);
 	opt->eating = sem_open("/eating", O_CREAT, S_IRWXU, 1);
+	if (opt->forks == SEM_FAILED || opt->write == SEM_FAILED || opt->eating == SEM_FAILED)
+        return (0);
+	return (1);
 }
 
 static int	initialize_philosophers(t_options *opt)
@@ -44,7 +47,8 @@ int	initialize_options(t_options *opt, char **argv)
 	opt->num_must_eat = -1;
 	if (argv[5])
 		opt->num_must_eat = ft_atoi(argv[5]);
-	initializes_semaphores(opt);
+	if (!initializes_semaphores(opt))
+		return (0);
 	return (1);
 }
 
--- philo_bonus/src/thread.c
+++ philo_bonus/src/thread.c
@@ -44,6 +44,17 @@ static void	join_and_close(t_options *opt, t_philo *philos, pthread_t tid)
 	sem_unlink("/write");
 	sem_unlink("/eating");
 }
+static void	p_close(t_options *opt)
+{
+	if (opt->philos)
+		free(opt->philos);
+	sem_close(opt->forks);
+	sem_close(opt->write);
+	sem_close(opt->eating);
+	sem_unlink("/forks");
+	sem_unlink("/write");
+	sem_unlink("/eating");
+}
 
 int		start_simulation(t_options *opt)
 {
@@ -68,7 +79,7 @@ int		start_simulation(t_options *opt)
 		pthread_detach(philo[0].t_id);
 		while (opt->dead == 0)
 			usleep(0);
-		return (1);
+		return (p_close(opt), 1);
 	}
 	join_and_close(opt, philo, tid);
 	return (1);
