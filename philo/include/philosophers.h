b68a74195df88dd2d6c9ba0b8a229078e66e4106
fernandezclaudia <cfernandezg@bcds.net>
2024-06-04 10:28:03 +0200

    fix header

--- philo/include/philosophers.h
+++ philo/include/philosophers.h
@@ -23,55 +23,52 @@ struct	s_options;
 // --------- PHILOSPHER ---------
 typedef struct	s_philosopher
 {
-	int		id;
-	pthread_t		tid;
-	int		left_fork;
-	int		right_fork;
-	int		is_eating;
-	uint64_t	time_of_death;
-	uint64_t	last_meal;
-	int		eat_count;
-	pthread_mutex_t	mtx;
-	pthread_mutex_t	mtx_eat;
+	int					id;
+	pthread_t			tid;
+	int					left_fork;
+	int					right_fork;
+	int					is_eating;
+	uint64_t			time_of_death;
+	uint64_t			last_meal;
+	int					eat_count;
+	pthread_mutex_t		mtx;
+	pthread_mutex_t		mtx_eat;
 	struct s_options	*opt;
-}			t_philo;
+}						t_philo;
 
 // ----------- OPTIONS ----------
 typedef struct	s_options
 {
-	int		num_philo;
-	uint64_t	time_to_die;
-	uint64_t	time_to_eat;
-	uint64_t	time_to_sleep;
-	int		num_must_eat;
-	uint64_t	start;
-	int		dead;
-	pthread_t	*tids;
-	t_philo		*philos;
+	int				num_philo;
+	uint64_t		time_to_die;
+	uint64_t		time_to_eat;
+	uint64_t		time_to_sleep;
+	int				num_must_eat;
+	uint64_t		start;
+	int				dead;
+	pthread_t		*tids;
+	t_philo			*philos;
 	pthread_mutex_t someone_dead;
 	pthread_mutex_t	*mtx_forks;
 	pthread_mutex_t	mtx_write;
 }		t_options;
+
 // ----------- OPTIONS -----------
-int	initialize_params(t_options *opt, char **argv);
+int			initialize_params(t_options *opt, char **argv);
+int			are_arguments_valid(int argc, char **argv);
 // ----------- THREADS ------------
-int	start_simulation(t_options *opt);
-// ............ monitor ...........
-void	*p_lifecycle(void *param);
-void	*have_eaten(void *param);
-// ........... destroy mtx ........
-void	destroy_mutexs(t_options *opt);
+int			start_simulation(t_options *opt);
+void		*p_lifecycle(void *param);
+void		*have_eaten(void *param);
+void		destroy_mutexs(t_options *opt);
 // ------------ ACTIONS -----------
-void	p_eat(t_options *opt, t_philo *philo);
-void	p_sleep(t_options *opt, t_philo *philo);
-void	p_think(t_options *opt, t_philo *philo);
-void	p_take_forks(t_options *opt, t_philo *philo);
-// --------- VALIDATION ----------
-int	are_arguments_valid(int argc, char **argv);
+void		p_eat(t_options *opt, t_philo *philo);
+void		p_sleep(t_options *opt, t_philo *philo);
+void		p_think(t_options *opt, t_philo *philo);
+void		p_take_forks(t_options *opt, t_philo *philo);
 // ------------- UTILS ------------
-void	print_action(int action, t_philo *philo, t_options *opt);
+long		ft_atoi(char *str);
+void		print_action(int action, t_philo *philo, t_options *opt);
 uint64_t	get_time(void);
-int	display_error();
-long	ft_atoi(char *str);
-void	ft_putnbr(int num);
+void		ft_putnbr(int num);
 #endif
